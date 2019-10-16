8db5bec56c4e269430d9b241f5d431afdc52b268
Ashleigh Christopher <ashleighc207@gmail.com>
2019-10-16 16:14:41 -0400

    add in a link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Link from "next/link";
 const App = () => {
   return (
     <div>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
       <h1>Index page</h1>
     </div>
   );
